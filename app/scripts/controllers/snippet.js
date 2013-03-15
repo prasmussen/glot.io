53978b9d64d5148789f71338e61ba0344f1b151e
Petter Rasmussen <petter.rasmussen@gmail.com>
2013-03-15 20:49:32 +0100

    Run button now works

--- app/scripts/controllers/snippet.js
+++ app/scripts/controllers/snippet.js
@@ -5,13 +5,27 @@ SnippetController.resolve = {
         return Snippets.get($route.current.params.id);
     },
 
+    results: function($route, Runs) {
+        return Runs.resultsBySnippet($route.current.params.id);
+    },
+
     settings: function($route, Settings) {
         return Settings.byLanguage($route.current.params.lang);
+    },
+
+    dbInfo: function(Couch) {
+        return Couch.db("api").info();
     }
 };
 
-function SnippetController($scope, $routeParams, $location, Utils, Segue, Snippets, Url, snippet, settings) {
+function SnippetController($scope, $routeParams, $location, Utils, Segue, Snippets, Runs, Url, snippet, results, settings, dbInfo) {
     var language = $routeParams.lang;
+
+    $scope.result = results.filter(function(res) {
+        return res.code === snippet.code.trimRight() && res.hasOwnProperty("result");
+    }).map(function(res) {
+        return res.result;
+    })[0];
 
     $scope.language = language;
 
@@ -33,7 +47,9 @@ function SnippetController($scope, $routeParams, $location, Utils, Segue, Snippe
     };
 
     $scope.run = function(code) {
-       $scope.result = "Run service currently not available.\nTry again later.";
+        $scope.result = {stdout: "Running..."};
+        Runs.create(language, code.trimRight(), snippet._id)
+            .success(waitForResult);
     };
 
     $scope.save = function(name, code) {
@@ -41,6 +57,12 @@ function SnippetController($scope, $routeParams, $location, Utils, Segue, Snippe
             .success(saveSuccess)
             .error(saveError);
     };
+
+    function waitForResult(doc) {
+        Runs.onResult(doc._id, dbInfo.update_seq, function(doc) {
+            $scope.result = doc.result;
+        });
+    }
 
     function saveSuccess(data) {
         $scope.alert = {
