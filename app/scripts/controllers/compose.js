c3ed3e1c6580440dff3fdd004288a4ea74591037
Petter Rasmussen <petter.rasmussen@gmail.com>
2013-03-21 20:42:32 +0100

    Added run button to compose view

--- app/scripts/controllers/compose.js
+++ app/scripts/controllers/compose.js
@@ -4,10 +4,14 @@ ComposeController.resolve = {
     // Inject language settings
     settings: function($route, Settings) {
         return Settings.byLanguage($route.current.params.lang);
+    },
+
+    dbInfo: function(Couch) {
+        return Couch.db("api").info();
     }
 };
 
-function ComposeController($scope, $routeParams, $location, Snippets, Utils, Segue, Url, settings) {
+function ComposeController($scope, $routeParams, $location, Snippets, Runs, Utils, Segue, Url, settings, dbInfo) {
     var language = $routeParams.lang;
     $scope.language = language;
 
@@ -28,11 +32,35 @@ function ComposeController($scope, $routeParams, $location, Snippets, Utils, Seg
         $scope.code = Utils.ensureMinLines("", 10);
     }
 
+    $scope.run = function(code) {
+        $scope.result = {stdout: "Running..."};
+
+        // Check if result already exists in the database
+        var r = Runs.resultByCode(language, code);
+
+        // Result was found
+        r.success(function(result) {
+            $scope.result = result;
+        });
+
+        // Result was not found, lets create a run request
+        r.error(function() {
+            Runs.create(language, code)
+                .success(waitForResult);
+        });
+    };
+
     $scope.save = function(name, code) {
         Snippets.create(language, name, code.trimRight())
             .success(saveSuccess)
             .error(saveError);
     };
+
+    function waitForResult(doc) {
+        Runs.onResult(doc._id, dbInfo.update_seq, function(doc) {
+            $scope.result = doc.result;
+        });
+    }
 
     function saveSuccess(data) {
         alertify.success("Snippet saved");
