ed5796cf82b965f916596f65249ffd78566643a3
Petter Rasmussen <petter.rasmussen@gmail.com>
2013-03-25 00:25:29 +0100

    Remove navbar-fixed-top, and cleanup containers

--- deploy/design.js
+++ deploy/design.js
@@ -35,7 +35,7 @@ db.save("_design/runs", {
         result_by_codehash: {
             map: function(doc) {
                 if (doc.type === "run" && doc.hasOwnProperty("result")) {
-                    // Skip results that dont have a stdout or stderr
+                    // Skip results that have neither a stdout or stderr
                     var r = doc.result;
                     if ((r.hasOwnProperty("stdout") && r.stdout) || (r.hasOwnProperty("stderr") && r.stderr)) {
                         emit([doc.language, doc.codehash], doc);
